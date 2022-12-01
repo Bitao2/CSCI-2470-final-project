429326e12ff55dc88d08b13c13ead48b1ee5de6f
James Ro <james_ro@brown.edu>
2022-11-30 22:09:14 -0500

    less layers

--- code/convolution_model.py
+++ code/convolution_model.py
@@ -33,23 +33,11 @@ def get_CNN_model(
     model = CustomSequential(
         [
         # conv layer with 16 filters
-        Conv2D(filters=16, kernel_size=(3,3), strides=(2, 2), padding = 'SAME', activation='leaky_relu'),
+        Conv2D_manual(filters=16, kernel_size=(3,3), strides=(2, 2), padding = 'SAME', activation='leaky_relu'),
         # batch normalization
         BatchNormalization(),
-        # conv layer with 32 filters
-        Conv2D(filters=32, kernel_size=(3,3), strides=(2, 2), padding = 'SAME', activation='leaky_relu'),
-        # batch normalization
-        BatchNormalization(),
-        # max pooling layer
-        tf.keras.layers.MaxPool2D(pool_size=(2,2)),
-        # conv layer with 64 filters
-        Conv2D(filters=64, kernel_size=(3,3), strides=(2, 2), padding = 'SAME', activation='leaky_relu'),
-        # batch normalization
-        BatchNormalization(),
-        # max pooling layer
-        tf.keras.layers.MaxPool2D(pool_size=(2,2)),
-        # conv layer with 128 filters
-        Conv2D(filters=128, kernel_size=(3,3), strides=(2, 2), padding = 'SAME', activation='leaky_relu'),
+        # conv layer with 100 filters
+        Conv2D(filters=100, kernel_size=(3,3), strides=(2, 2), padding = 'SAME', activation='leaky_relu'),
         # batch normalization
         BatchNormalization(),
         # max pooling layer
@@ -61,12 +49,9 @@ def get_CNN_model(
         # dropout layer
         Dropout(rate = 0.1),
         # dense layer
-        tf.keras.layers.Dense(64, activation='leaky_relu'),
+        tf.keras.layers.Dense(100, activation='leaky_relu'),
         # dropout layer
-        Dropout(rate=0.2),
-        # second dense layer
-        tf.keras.layers.Dense(32, activation='leaky_relu'),
-        Dropout(rate=0.2),
+        Dropout(rate=0.1),
         # final dense layer using softmax
         tf.keras.layers.Dense(10, activation='softmax')
         ], input_prep_fn=input_prep_fn, output_prep_fn=output_prep_fn, augment_fn=augment_fn
